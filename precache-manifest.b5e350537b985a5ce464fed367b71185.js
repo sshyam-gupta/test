f5a7e0640539ffd197997ed4642ca03c40691a0e
sshyam-gupta <38554977+sshyam-gupta@users.noreply.github.com>
2019-12-25 18:43:18 +0000

    Deploying to gh-pages from  9d439db3429cacf84bfa131acabe21115effeb45

---
+++ precache-manifest.b5e350537b985a5ce464fed367b71185.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c279125d63d50b46c2b063de8a751d2",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "c9e1d434e9bc2b1b60a7",
+    "url": "/test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c88cebed7dec8a545eeb",
+    "url": "/test/static/js/2.250dc4af.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/test/static/js/2.250dc4af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c9e1d434e9bc2b1b60a7",
+    "url": "/test/static/js/main.8c0e16cf.chunk.js"
+  },
+  {
+    "revision": "c5270fcaa5748eecb903",
+    "url": "/test/static/js/runtime-main.a0b69a2a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
